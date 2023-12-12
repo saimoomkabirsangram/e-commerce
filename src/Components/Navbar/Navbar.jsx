5a3e54a837205aefc9fd845d53defde1cf938909
Delower Hossain <108389033+delower68@users.noreply.github.com>
2023-12-13 06:17:28 +0800

    sepical edition img fixed and completed all writen part

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { FiUser } from "react-icons/fi";
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar lg:p-5 p-2 text-white bg-[#2b80ad] ">
+      <div className="navbar lg:p-5 p-2 text-white bg-[#2d6588] ">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
--- src/Components/SpecialEdition/SpecialEdition.jsx
+++ src/Components/SpecialEdition/SpecialEdition.jsx
@@ -1,35 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import './SpecialEdition.css'
 const SpecialEdition = () => {
   return (
-    // <div className="lg:p-16 bg-cyan-600/20 text-black">
-    //   <div className="bg-[url('https://i.ibb.co/KsnQxnz/10024.jpg')] lg:h-[600px] lg:w-full bg-cover">
-    //     <div className="flex flex-col lg:justify-start lg:items-start sm:justify-center sm:items-center">
-    //       <h3>Limited Time Offer</h3>
-    //       <h1>Special Edition</h1>
-    //       <h2>
-    //         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
-    //         tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-    //       </h2>
-    //       <h2>Buy This T-shirt At 20% Discount, Use Code OFF20</h2>
-    //       <Link>
-    //         <button
-    //           className="rounded lg:px-8 lg:py-3 hover:bg-black
-    //       hover:text-white font-bold py-2 text-black bg-white"
-    //         >
-    //           Shop Now
-    //         </button>
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className="lg:p-16 p-5 bg-cyan-600/20 text-black">
-      <div className="bg-[url('https://i.ibb.co/KsnQxnz/10024.jpg')] lg:h-[600px] h-[300px] lg:w-full bg-cover ">
-        <div className="lg:py-44 lg:px-0 pt-10  lg:text-start text-center lg:p-0 p-5">
+    <div className="lg:py-16 p-5 bg-cyan-600/20 text-black">
+      <div className="header-imgs lg:h-[400px] h-[200px]  bg-cover ">
+        <div className="lg:py-32 lg:px-0 py-28  lg:text-start text-center lg:p-0 p-5">
           <div className="headerText flex flex-col lg:justify-start lg:items-start justify-center items-center lg:ms-28">
-            <p className="font-mono text-white text-2xl lg:mb-5 mb-2">Limited Time Offer!</p>
-            <h1 className=" text-white text-4xl lg:text-5xl font-bold">
+            <p className="font-mono text-white font-bold text-xl lg:mb-5 mb-2">Limited Time Offer!</p>
+            <h1 className=" text-white text-3xl lg:text-5xl font-bold">
             Special Edition
             </h1>
             <p className="text-white lg:w-1/2 w-4/5 mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
